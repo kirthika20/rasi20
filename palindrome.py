3595f5a140dac774f3bda2f395824a2fbea2e077
kirthika20 <52449551+kirthika20@users.noreply.github.com>
2019-07-05 10:27:36 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,9 +2,9 @@ n=int(input())
 temp=n
 reverse=0
 while(n>0):
-dig=n%10
-reverse=reverse*10+dig
-n=n/10
+  dig=n%10
+  reverse=reverse*10+dig
+  n=n/10
 if(temp==reverse):
   print("palindrome")
 else:
